6205b2c5c4e936af4d18eddd0ddb849293b96db6
Jo Wagner <jowagner@limago.de>
2023-07-20 14:30:57 +0200

    initial commit

--- ModulUebung/de.msg.provider/src/de/msg/provider/StringSupplier.java
+++ ModulUebung/de.msg.provider/src/de/msg/provider/StringSupplier.java
@@ -6,7 +6,7 @@ import java.util.function.Supplier;
 public interface StringSupplier extends Supplier<String> {
 
     static StringSupplier create() {
-        ServiceLoader<StringSupplier> loader = ServiceLoader.load(StringSupplier.class);
+        var loader = ServiceLoader.load(StringSupplier.class);
         return loader.findFirst().get();
     }
 }
